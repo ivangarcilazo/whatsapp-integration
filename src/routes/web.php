507dbb3adaf6f29a4d48c5e3e8866a615f68bf02
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2025-07-18 18:53:39 -0300

    Added route for get phone status number

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,10 +2,30 @@
 
 use EglobalOneLab\WhatsappIntegration\Http\Controllers\BitrixController;
 use EglobalOneLab\WhatsappIntegration\Http\Controllers\WhatsappIntegrationClearController;
+use EglobalOneLab\WhatsappIntegration\Models\WhatsappIntegration;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('/whatsapp-integration-webhook', [\EglobalOneLab\WhatsappIntegration\Http\Controllers\TwilioController::class, 'webhook']);
 
 Route::post('/whatsapp-integration-history', [BitrixController::class, 'index']);
 
 Route::post('/whatsapp-integration-clear', [WhatsappIntegrationClearController::class, 'store']);
+
+Route::get('/whatsapp-integration-get-user-integration/{phone_number}', function (Request $request, $phone_number) {
+    $phone = str_replace('+', '', (string) $phone_number);
+
+    $user = WhatsappIntegration::where('whatsapp_id', $phone)->first();
+
+    if (!$user) {
+        return response()->json([
+            'success' => 404,
+            'message' => 'No integration found for that phone number'
+        ], 404);
+    }
+
+    return response()->json([
+        'success' => 200,
+        'user' => $user->toArray()
+    ]);
+});
